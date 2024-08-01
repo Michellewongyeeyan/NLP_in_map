4f9685c35b42afed3b861dda7df7494e03a445c8
AnsonCar <anson1tsang@gmail.com>
2024-08-01 08:27:42 +0800

    update docker

--- backend/db_api_ninja/main.py
+++ backend/db_api_ninja/main.py
@@ -9,5 +9,8 @@ PORT = int(os.getenv("DJANGO_PORT", 8000))
 DEBUG = bool(os.getenv("DJANGO_DEBUG", True))
 
 if __name__ == "__main__":
-    # print(f"http://127.0.0.1:{PORT}")
+    print('-'*50)
+    print(f'ANSC:\t  API SERVER RUN ON 127.0.0.1:{PORT}')
+    print(f"ANSC:\t  API SERVER DEBUG MODE: {DEBUG}")
+    print('-'*50)
     uvicorn.run("db_api_ninja.asgi:application", host="0.0.0.0", port=PORT, reload=DEBUG)
