37dcfdb8f59b4b1f6897d39941f8124b7396a31d
AnsonCar <anson1tsang@gmail.com>
2024-08-04 16:12:52 +0800

    add nuxt leaflet

---
+++ frontend/nuxt-ts-tailwind/components/map/Map.vue
@@ -0,0 +1,14 @@
+<!-- https://leaflet.nuxtjs.org/ -->
+<template>
+    <div style="height:100%; width:100%">
+        <LMap ref="map" :zoom="zoom" :center="[47.21322, -1.559482]" :use-global-leaflet="false">
+            <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
+                layer-type="base" name="OpenStreetMap" />
+        </LMap>
+    </div>
+</template>
+
+<script setup>
+const zoom = ref(6)
+</script>
