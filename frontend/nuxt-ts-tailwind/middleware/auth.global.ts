c8fb528bd4752d4a68b5b8a600c9159906f1a637
AnsonCar <anson1tsang@gmail.com>
2024-08-04 17:36:26 +0800

    update path

--- frontend/nuxt-ts-tailwind/middleware/auth.global.ts
+++ frontend/nuxt-ts-tailwind/middleware/auth.global.ts
@@ -2,5 +2,5 @@ export default defineNuxtRouteMiddleware((to, from) => {
   let user = useState('auth');
   if (user.value && to.path === '/login') return navigateTo('/');
   if (!user.value && to.path !== '/login') return navigateTo('/login');
-  if (to.path === '/') return navigateTo('/account');
+  if (to.path === '/') return navigateTo('/chapters/1');
 });
