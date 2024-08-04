4a93c4f5741cffd0b53fa73b4d2f21d9edd22f6a
AnsonCar <anson1tsang@gmail.com>
2024-08-05 00:25:11 +0800

    add location label

--- frontend/nuxt-ts-tailwind/components/map/Map.vue
+++ frontend/nuxt-ts-tailwind/components/map/Map.vue
@@ -1,11 +1,18 @@
 <!-- https://leaflet.nuxtjs.org/ -->
 <template>
-  <div style="height:100%; width:100%">
-    <LMap ref="map" :zoom="zoom" :center="[47.21322, -1.559482]" :use-global-leaflet="false">
+  <div class="size-full">
+    <LMap ref="map" :zoom="zoom" :center="[52.21322, -0.359482]" :use-global-leaflet="false" autoZIndex>
       <LTileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution="&amp;copy; <a href=&quot;https://www.openstreetmap.org/&quot;>OpenStreetMap</a> contributors"
         layer-type="base" name="OpenStreetMap" />
-      <LMarker :lat-lng="data.nominatim" draggable v-for="data, index in props.datas" :key="index" />
+      <LMarker :lat-lng="data.nominatim" draggable v-for="data, index in props.datas" :key="index">
+        <LPopup>{{ data.location }}
+          <br>
+          <div v-for="txt, textIndex in data.text">
+            <p><b>{{ textIndex + 1 }}.</b> {{ txt }}</p>
+          </div>
+        </LPopup>
+      </LMarker>
     </LMap>
   </div>
 </template>
